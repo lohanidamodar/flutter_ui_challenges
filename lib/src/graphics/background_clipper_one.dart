27c06b46367f68b632f7d557b401a32538005d54
Damodar Lohani <dlohani48@gmail.com>
2018-12-21 15:16:57 +0545

    adding curve to another corner and cleaning up

--- lib/src/graphics/background_clipper_one.dart
+++ lib/src/graphics/background_clipper_one.dart
@@ -4,8 +4,12 @@ class BackgroundClipperOne extends CustomClipper<Path> {
   @override
     Path getClip(Size size) {
       final path = Path()
-        ..lineTo(0.0, size.height-10.0)
+        ..lineTo(0.0, size.height)
+        ..lineTo(size.width, size.height)
         ..lineTo(size.width, 0.0)
+        ..quadraticBezierTo(size.width, 0.0, size.width-20.0, 0.0)
+        ..lineTo(40.0, 70.0)
+        ..quadraticBezierTo(10.0, 85.0, 0.0, 120.0)
         ..close();
       return path;
     }
--- lib/src/pages/login/login2.dart
+++ lib/src/pages/login/login2.dart
@@ -31,65 +31,68 @@ class LoginTwoPage extends StatelessWidget {
           padding: EdgeInsets.all(20.0),
           child: Stack(
             children: <Widget>[
-              Container(
-                height: 400,
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(height: 90.0,),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                      child: TextField(
-                        style: TextStyle(color: Colors.blue),
-                        decoration: InputDecoration(
-                          hintText: "Email address",
-                          hintStyle: TextStyle(color: Colors.blue.shade200),
-                          border: InputBorder.none,
-                          icon: Icon(Icons.lock, color: Colors.blue,)
-                        ),
-                      )
-                    ),
-                    Container(child: Divider(color: Colors.blue.shade400,), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0),),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                      child: TextField(
-                        style: TextStyle(color: Colors.blue),
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.blue.shade200),
-                          border: InputBorder.none,
-                          icon: Icon(Icons.lock, color: Colors.blue,)
-                        ),
-                      )
-                    ),
-                    Container(child: Divider(color: Colors.blue.shade400,), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0),),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(padding: EdgeInsets.only(right: 20.0),
-                          child: Text("Forget Password",
-                            style: TextStyle(color: Colors.black45),
-                          )
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 10.0,),
-                    
-                  ],
-                ),
-              ),
               ClipPath(
                 clipper: BackgroundClipperOne(),
                 child: Container(
-                  height: 120,
-                  color: Colors.blue.shade100,
+                  height: 400,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 90.0,),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.blue),
+                          decoration: InputDecoration(
+                            hintText: "Email address",
+                            hintStyle: TextStyle(color: Colors.blue.shade200),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.lock, color: Colors.blue,)
+                          ),
+                        )
+                      ),
+                      Container(child: Divider(color: Colors.blue.shade400,), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0),),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.blue),
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.blue.shade200),
+                            border: InputBorder.none,
+                            icon: Icon(Icons.lock, color: Colors.blue,)
+                          ),
+                        )
+                      ),
+                      Container(child: Divider(color: Colors.blue.shade400,), padding: EdgeInsets.only(left: 20.0,right: 20.0, bottom: 10.0),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Container(padding: EdgeInsets.only(right: 20.0),
+                            child: Text("Forget Password",
+                              style: TextStyle(color: Colors.black45),
+                            )
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10.0,),
+                      
+                    ],
+                  ),
                 ),
               ),
+              // ClipPath(
+              //   clipper: BackgroundClipperOne(),
+              //   child: Container(
+              //     height: 120,
+              //     color: Colors.blue.shade100,
+              //   ),
+              // ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
