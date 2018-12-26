27a5e2b68b8f6e9fedb27213d9bbc08ae48bdd21
Damodar Lohani <dlohani48@gmail.com>
2018-12-26 08:27:10 +0545

    cart page

---
+++ lib/src/pages/grocery/gwidgets/glistitem3.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/src/pages/grocery/gwidgets/gtypography.dart';
+
+class GroceryListItemThree extends StatelessWidget {
+  const GroceryListItemThree({
+    Key key,
+    @required this.title,
+    @required this.subtitle,
+    @required this.image,
+    this.price,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String image;
+  final double price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Container(
+            height: 80.0,
+            child: Image.asset(image, height: 80.0,)),
+          title: Container(
+            height: 120.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new GroceryTitle(text: title),
+                new GrocerySubtitle(text: subtitle)
+              ],
+            ),
+          ),
+          trailing: Column(
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.add_circle), color: Colors.green, onPressed: (){},),
+              Text("1", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold),),
+              IconButton(icon: Icon(Icons.remove_circle,), color: Colors.green, onPressed: (){},)
+            ],
+          ),
+        ),
+      );
+  }
+}
--- lib/src/pages/grocery/tabs/gcarttab.dart
+++ lib/src/pages/grocery/tabs/gcarttab.dart
@@ -1,8 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/src/pages/grocery/gwidgets/glistitem3.dart';
+
 
 class GroceryCartTabView extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
-      return Center(child: Text("Cart tab"),);
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                GroceryListItemThree(
+                  image: "assets/grocery/pineapple.png",
+                  subtitle: "4 in a pack",
+                  title: "Pineapple",
+                ),
+                GroceryListItemThree(
+                  image: "assets/grocery/cabbage.png",
+                  subtitle: "1 kg",
+                  title: "cabbage",
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 170,
+            color: Colors.black12,
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Subtotal"),
+                    Text("Rs. 1500"),
+                  ],
+                ),
+                SizedBox(height: 10.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Delivery fee"),
+                    Text("Rs. 100"),
+                  ],
+                ),
+                SizedBox(height: 10.0,),  
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Total"),
+                    Text("Rs. 1600"),
+                  ],
+                ),
+                SizedBox(height: 10.0,),
+                RaisedButton(
+                  color: Colors.green,
+                  onPressed: (){},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text("Continue to Checkout"),
+                      Text("Rs. 1600")
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      );
     }
 }
