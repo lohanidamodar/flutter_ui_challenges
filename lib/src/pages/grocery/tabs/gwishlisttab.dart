372e1c31873aa531be20f34cb064efea6852fdbb
Damodar Lohani <dlohani48@gmail.com>
2018-12-27 06:30:32 +0545

    wishlist page

--- lib/src/pages/grocery/tabs/gwishlisttab.dart
+++ lib/src/pages/grocery/tabs/gwishlisttab.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/src/pages/grocery/gwidgets/glistitem3.dart';
 
 class GroceryWishlistTabView extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
-      return Center(child: Text("Wishlist tab"),);
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(10.0),
+              children: <Widget>[
+                GroceryListItemThree(
+                  image: "assets/grocery/pineapple.png",
+                  subtitle: "4 in a pack",
+                  title: "Pineapple",
+                ),
+                GroceryListItemThree(
+                  image: "assets/grocery/cabbage.png",
+                  subtitle: "1 kg",
+                  title: "cabbage",
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          _buildTotals()
+        ],
+      );
     }
+
+  Widget _buildTotals() {
+    return Container(
+      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+      child: RaisedButton(
+        color: Colors.green,
+        onPressed: (){},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text("Add to Wishlist", style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      )
+    );
+  }
 }
