980fcfbb42644d926bd413dfcafa3be9db529b4e
Damodar Lohani <dlohani48@gmail.com>
2018-12-27 06:39:04 +0545

    profile tab

--- lib/src/pages/grocery/tabs/gprofiletab.dart
+++ lib/src/pages/grocery/tabs/gprofiletab.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/src/pages/grocery/gwidgets/gtypography.dart';
 
 class GroceryProfileTabView extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
-      return Center(child: Text("Profile tab"),);
+      return Container(
+        color: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.edit),
+              title: GroceryTitle(text: "My Orders"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.favorite_border),
+              title: GroceryTitle(text: "My Favorite"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.account_balance_wallet),
+              title: GroceryTitle(text: "Payment Details"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.settings),
+              title: GroceryTitle(text: "My Account"),
+            ),
+          ],
+        ),
+      );
     }
 }
