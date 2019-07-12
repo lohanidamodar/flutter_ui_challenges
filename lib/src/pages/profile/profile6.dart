481769577ed5d6353289d29a87a729ce40198b8d
root <root@localhost.localdomain>
2019-07-12 22:01:10 +0545

    profile 6 added

---
+++ lib/src/pages/profile/profile6.dart
@@ -0,0 +1,273 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter_ui_challenges/src/widgets/main_menu.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:flutter/material.dart';
+
+class ProfileSixPage extends StatelessWidget {
+  ProfileSixPage(this.dev);
+
+  final DEV dev;
+
+  Widget _buildContent(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildAvatar(),
+          _buildInfo(),
+          _buildVideoScroller(),
+          _buildbuton(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildbuton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+          color: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Text(
+            'Back',
+            style: TextStyle(color: Colors.white70),
+          ),
+          onPressed: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => MainMenu()));
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Container(
+      width: 110.0,
+      height: 110.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.white30),
+      ),
+      margin: const EdgeInsets.only(top: 32.0, left: 16.0),
+      padding: const EdgeInsets.all(3.0),
+      child: ClipOval(
+        child: Image.asset(dev.avatar),
+      ),
+    );
+  }
+
+  Widget _buildInfo() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            dev.firstName + dev.lastName,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+          ),
+          Text('\n'),
+          Text(
+            dev.location,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.85),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Container(
+            color: Colors.white.withOpacity(0.85),
+            margin: const EdgeInsets.symmetric(vertical: 16.0),
+            width: 225.0,
+            height: 1.0,
+          ),
+          Text(
+            dev.biography,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.85),
+              height: 1.4,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildVideoScroller() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: SizedBox.fromSize(
+        size: Size.fromHeight(245.0),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          itemCount: dev.videos.length,
+          itemBuilder: (BuildContext context, int index) {
+            var video = dev.videos[index];
+
+            return VideoCard(video);
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              size: 50.0,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          Image.asset(dev.backdropPhoto, fit: BoxFit.cover),
+          BackdropFilter(
+            filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: _buildContent(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VideoCard extends StatelessWidget {
+  VideoCard(this.video);
+
+  final Video video;
+
+  BoxDecoration _buildShadowAndRoundedCorners() {
+    return BoxDecoration(
+      color: Colors.white.withOpacity(0.4),
+      borderRadius: BorderRadius.circular(10.0),
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          spreadRadius: 2.0,
+          blurRadius: 10.0,
+          color: Colors.black26,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildThumbnail() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: Stack(
+        children: <Widget>[
+          Image.asset(video.thumbnail),
+          Positioned(
+            bottom: 12.0,
+            right: 12.0,
+            child: _buildPlayButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlayButton() {
+    return Material(
+      color: Colors.black87,
+      type: MaterialType.circle,
+      child: InkWell(
+        onTap: () async {
+          if (await canLaunch(video.url)) {
+            await launch(video.url);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            Icons.play_arrow,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfo() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0, left: 4.0, right: 4.0),
+      child: Text(
+        video.title,
+        style: TextStyle(color: Colors.white.withOpacity(0.85)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175.0,
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+      decoration: _buildShadowAndRoundedCorners(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Flexible(flex: 3, child: _buildThumbnail()),
+          Flexible(flex: 2, child: _buildInfo()),
+        ],
+      ),
+    );
+  }
+}
+
+class DEV {
+  DEV({
+    @required this.firstName,
+    @required this.lastName,
+    @required this.avatar,
+    @required this.backdropPhoto,
+    @required this.location,
+    @required this.biography,
+    @required this.videos,
+  });
+
+  final String firstName;
+
+  final String lastName;
+
+  final String avatar;
+
+  final String backdropPhoto;
+
+  final String location;
+
+  final String biography;
+
+  final List<Video> videos;
+}
+
+class Video {
+  Video({
+    @required this.title,
+    @required this.thumbnail,
+    @required this.url,
+  });
+
+  final String title;
+
+  final String thumbnail;
+
+  final String url;
+}
--- lib/src/widgets/main_menu.dart
+++ lib/src/widgets/main_menu.dart
@@ -12,6 +12,7 @@ import 'package:flutter_ui_challenges/src/pages/onboarding/intro2.dart';
 import 'package:flutter_ui_challenges/src/pages/onboarding/intro3.dart';
 import 'package:flutter_ui_challenges/src/pages/onboarding/intro4.dart';
 import 'package:flutter_ui_challenges/src/pages/profile/profile5.dart';
+import 'package:flutter_ui_challenges/src/pages/profile/profile6.dart';
 import 'package:flutter_ui_challenges/src/pages/quiz_app/check_answers.dart';
 import 'package:flutter_ui_challenges/src/pages/quiz_app/demo_values.dart';
 import 'package:flutter_ui_challenges/src/pages/quiz_app/home.dart';
@@ -79,6 +80,7 @@ class _MainMenuState extends State<MainMenu> {
       SubMenuItem("Profile Three", ProfileThreePage()),
       SubMenuItem("Profile Four", ProfileFourPage()),
       SubMenuItem("Profile Five", ProfileFivePage()),
+      SubMenuItem("Profile six", ProfileSixPage(sid)),
     ]),
     MenuItem(title: "Ecommerce", icon: Icons.shopping_basket, items: [
       SubMenuItem("Ecommerce One", EcommerceOnePage()),
@@ -96,7 +98,7 @@ class _MainMenuState extends State<MainMenu> {
       SubMenuItem("Article Two", ArticleTwoPage()),
     ]),
     MenuItem(title: "Food", icon: Icons.fastfood, items: [
-      SubMenuItem("Fruits Add to Cart", AvocadoPage() ),
+      SubMenuItem("Fruits Add to Cart", AvocadoPage()),
       SubMenuItem("Cake Details", CakePage()),
       SubMenuItem("Recipe List", RecipeListPage()),
       SubMenuItem("Recipe Single", RecipeSinglePage()),
@@ -105,9 +107,24 @@ class _MainMenuState extends State<MainMenu> {
     ]),
     MenuItem(title: "Quiz app", icon: FontAwesomeIcons.question, items: [
       SubMenuItem("Quiz Home", QuizHomePage()),
-      SubMenuItem("Quiz Page", QuizPage(questions: demoQuestions, category: categories[9],)),
-      SubMenuItem("Quiz Result", QuizFinishedPage(questions: demoQuestions, answers: demoAnswers,)),
-      SubMenuItem("Check Answers", CheckAnswersPage(questions: demoQuestions, answers: demoAnswers,)),
+      SubMenuItem(
+          "Quiz Page",
+          QuizPage(
+            questions: demoQuestions,
+            category: categories[9],
+          )),
+      SubMenuItem(
+          "Quiz Result",
+          QuizFinishedPage(
+            questions: demoQuestions,
+            answers: demoAnswers,
+          )),
+      SubMenuItem(
+          "Check Answers",
+          CheckAnswersPage(
+            questions: demoQuestions,
+            answers: demoAnswers,
+          )),
     ]),
     MenuItem(title: "Todo", icon: FontAwesomeIcons.tasks, items: [
       SubMenuItem("Todo Home One", TodoHomeOnePage()),
@@ -140,13 +157,12 @@ class _MainMenuState extends State<MainMenu> {
       SubMenuItem("Loaders", LoadersPage()),
     ]),
     SubMenuItem("Grocery UI Kit", GroceryHomePage())
-
   ];
 
-  Map<String,bool> viewData = <String,bool>{};
+  Map<String, bool> viewData = <String, bool>{};
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     _getViewData();
   }
@@ -154,7 +170,10 @@ class _MainMenuState extends State<MainMenu> {
   _getViewData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      viewData = Map<String,bool>.from(json.decode(prefs.getString('page_view_data') != null ? prefs.getString('page_view_data') : "{}"));
+      viewData = Map<String, bool>.from(json.decode(
+          prefs.getString('page_view_data') != null
+              ? prefs.getString('page_view_data')
+              : "{}"));
     });
     return;
   }
@@ -166,55 +185,65 @@ class _MainMenuState extends State<MainMenu> {
 
   @override
   Widget build(BuildContext context) => Drawer(
-    child: Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100),
-        child: Container(
-          padding: EdgeInsets.only(top: 30.0),
-          height: 100,
-          color: Colors.deepOrange,
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Image.asset('assets/icon/icon.png',width: 20,),
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child: Container(
+              padding: EdgeInsets.only(top: 30.0),
+              height: 100,
+              color: Colors.deepOrange,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image.asset(
+                    'assets/icon/icon.png',
+                    width: 20,
+                  ),
+                ),
+                title: Text("Flutter UIs",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0)),
+              ),
             ),
-            title: Text("Flutter UIs", style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0
-            )),
+          ),
+          body: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              dynamic page = pages[index];
+              return page is MenuItem
+                  ? _buildExpandableMenu(page, context)
+                  : _buildSubMenu(page, context);
+            },
+            itemCount: pages.length,
           ),
         ),
-      ),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (BuildContext context,int index){
-          dynamic page = pages[index];
-          return page is MenuItem ? _buildExpandableMenu(page, context) : _buildSubMenu(page, context);
-        },
-        itemCount: pages.length,
-      ),
-    ),
-  );
+      );
 
   Widget _buildExpandableMenu(MenuItem page, BuildContext context) {
     bool hasChanges = false;
     page.items.forEach((item) {
-      if(viewData[item.title] != true) {
+      if (viewData[item.title] != true) {
         hasChanges = true;
         return;
       }
     });
     return ExpansionTile(
       leading: Icon(page.icon),
-      title: Text("${page.title} (${page.items.length} layouts)", style: TextStyle(fontWeight: FontWeight.bold, color: hasChanges ? Colors.deepOrange: Colors.black87),),
+      title: Text(
+        "${page.title} (${page.items.length} layouts)",
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: hasChanges ? Colors.deepOrange : Colors.black87),
+      ),
       children: _buildSubMenus(page.items, context),
     );
   }
 
   List<Widget> _buildSubMenus(List<SubMenuItem> items, BuildContext context) {
     final List<Widget> subMenus = [];
-    items.forEach((item)=>subMenus.add(_buildSubMenu(item, context)));
+    items.forEach((item) => subMenus.add(_buildSubMenu(item, context)));
     return subMenus;
   }
 
@@ -226,8 +255,14 @@ class _MainMenuState extends State<MainMenu> {
         contentPadding: EdgeInsets.all(0),
         dense: false,
         isThreeLine: false,
-        title: Text(item.title, style: Theme.of(context).textTheme.subhead.copyWith(color: viewData[item.title] != true ? Colors.deepOrange : Colors.black87),),
-        onTap: () => _openPage(item.title,item.page, context),
+        title: Text(
+          item.title,
+          style: Theme.of(context).textTheme.subhead.copyWith(
+              color: viewData[item.title] != true
+                  ? Colors.deepOrange
+                  : Colors.black87),
+        ),
+        onTap: () => _openPage(item.title, item.page, context),
       ),
     );
   }
@@ -237,11 +272,7 @@ class _MainMenuState extends State<MainMenu> {
       viewData[title] = true;
     });
     _writeViewData();
-    Navigator.push(context, 
-      MaterialPageRoute(
-        builder: (context) => page
-      )
-    );
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
   }
 }
 
@@ -250,16 +281,50 @@ class MenuItem {
   final List<SubMenuItem> items;
   final IconData icon;
 
-
-  MenuItem({Key key, @required this.title, this.items, this.icon=Icons.label_important});
-
-
+  MenuItem(
+      {Key key,
+      @required this.title,
+      this.items,
+      this.icon = Icons.label_important});
 }
+
 class SubMenuItem {
   final String title;
   final Widget page;
   final IconData icon;
 
   SubMenuItem(this.title, this.page, {this.icon = Icons.block});
-
 }
+
+final DEV sid = DEV(
+  firstName: 'Cimple',
+  lastName: 'Sid',
+  avatar: 'assets/img/avatar.png',
+  backdropPhoto: 'assets/img/backdrop.png',
+  location: 'Mahendranagar, Nepal',
+  biography: 'Siddhartha  Joshi is a Flutter dev  '
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  '
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  videos: <Video>[
+    Video(
+      title: 'WIFI hacking part 1',
+      thumbnail: 'assets/img/video1_thumb.png',
+      url: 'https://www.youtube.com/watch?v=06qoTsKYWKE',
+    ),
+    Video(
+      title: 'WIFI hacking part 2',
+      thumbnail: 'assets/img/video2_thumb.png',
+      url: 'https://www.youtube.com/watch?v=3XG4c5_mGCM',
+    ),
+    Video(
+      title: 'WIFI hacking part 3',
+      thumbnail: 'assets/img/video3_thumb.png',
+      url: 'https://www.youtube.com/watch?v=C29QstsxWQE',
+    ),
+    Video(
+      title: 'Find facebook users location',
+      thumbnail: 'assets/img/video4_thumb.png',
+      url: 'https://www.youtube.com/watch?v=J9zhKtL_gH0',
+    ),
+  ],
+);
