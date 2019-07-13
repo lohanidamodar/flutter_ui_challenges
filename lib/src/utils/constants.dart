bb82621e43dd1d620cb055da8dd665c8ee1adf64
Damodar Lohani <dlohani48@gmail.com>
2019-07-13 06:18:32 +0545

    refactoring

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_ui_challenges/src/pages/profile/profile6.dart';
+
+final DEV sid = DEV(
+  firstName: 'Cimple',
+  lastName: 'Sid',
+  avatar: 'assets/img/avatar.png',
+  backdropPhoto: 'assets/img/backdrop.png',
+  location: 'Mahendranagar, Nepal',
+  biography: 'Siddhartha  Joshi is a Flutter dev  '
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  '
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  videos: <Video>[
+    Video(
+      title: 'WIFI hacking part 1',
+      thumbnail: 'assets/img/video1_thumb.png',
+      url: 'https://www.youtube.com/watch?v=06qoTsKYWKE',
+    ),
+    Video(
+      title: 'WIFI hacking part 2',
+      thumbnail: 'assets/img/video2_thumb.png',
+      url: 'https://www.youtube.com/watch?v=3XG4c5_mGCM',
+    ),
+    Video(
+      title: 'WIFI hacking part 3',
+      thumbnail: 'assets/img/video3_thumb.png',
+      url: 'https://www.youtube.com/watch?v=C29QstsxWQE',
+    ),
+    Video(
+      title: 'Find facebook users location',
+      thumbnail: 'assets/img/video4_thumb.png',
+      url: 'https://www.youtube.com/watch?v=J9zhKtL_gH0',
+    ),
+  ],
+);
--- lib/src/widgets/main_menu.dart
+++ lib/src/widgets/main_menu.dart
@@ -19,6 +19,7 @@ import 'package:flutter_ui_challenges/src/pages/quiz_app/home.dart';
 import 'package:flutter_ui_challenges/src/pages/quiz_app/quiz_finished.dart';
 import 'package:flutter_ui_challenges/src/pages/quiz_app/quiz_page.dart';
 import 'package:flutter_ui_challenges/src/pages/todo/todo_home1.dart';
+import 'package:flutter_ui_challenges/src/utils/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -67,7 +68,15 @@ class _MainMenuState extends State<MainMenu> {
     MenuItem(title: "Animations", icon: FontAwesomeIcons.truckMoving, items: [
       SubMenuItem("Hero Animation", AnimationOnePage()),
     ]),
-    MenuItem(title: "User flow", icon: Icons.person, items: [
+    MenuItem(title: "Profile",icon: Icons.person, items: [
+      SubMenuItem("Profile One", ProfileOnePage()),
+      SubMenuItem("Profile Two", ProfileTwoPage()),
+      SubMenuItem("Profile Three", ProfileThreePage()),
+      SubMenuItem("Profile Four", ProfileFourPage()),
+      SubMenuItem("Profile Five", ProfileFivePage()),
+      SubMenuItem("Profile six", ProfileSixPage(sid)),
+    ]),
+    MenuItem(title: "Autnentication", icon: Icons.lock, items: [
       SubMenuItem("Auth One", AuthOnePage()),
       SubMenuItem("Login One", LoginOnePage()),
       SubMenuItem("Login Two", LoginTwoPage()),
@@ -75,12 +84,6 @@ class _MainMenuState extends State<MainMenu> {
       SubMenuItem("Login Four", LoginFourPage()),
       SubMenuItem("Login Five", LoginFivePage()),
       SubMenuItem("Signup One", SignupOnePage()),
-      SubMenuItem("Profile One", ProfileOnePage()),
-      SubMenuItem("Profile Two", ProfileTwoPage()),
-      SubMenuItem("Profile Three", ProfileThreePage()),
-      SubMenuItem("Profile Four", ProfileFourPage()),
-      SubMenuItem("Profile Five", ProfileFivePage()),
-      SubMenuItem("Profile six", ProfileSixPage(sid)),
     ]),
     MenuItem(title: "Ecommerce", icon: Icons.shopping_basket, items: [
       SubMenuItem("Ecommerce One", EcommerceOnePage()),
@@ -295,36 +298,3 @@ class SubMenuItem {
 
   SubMenuItem(this.title, this.page, {this.icon = Icons.block});
 }
-
-final DEV sid = DEV(
-  firstName: 'Cimple',
-  lastName: 'Sid',
-  avatar: 'assets/img/avatar.png',
-  backdropPhoto: 'assets/img/backdrop.png',
-  location: 'Mahendranagar, Nepal',
-  biography: 'Siddhartha  Joshi is a Flutter dev  '
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  '
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-  videos: <Video>[
-    Video(
-      title: 'WIFI hacking part 1',
-      thumbnail: 'assets/img/video1_thumb.png',
-      url: 'https://www.youtube.com/watch?v=06qoTsKYWKE',
-    ),
-    Video(
-      title: 'WIFI hacking part 2',
-      thumbnail: 'assets/img/video2_thumb.png',
-      url: 'https://www.youtube.com/watch?v=3XG4c5_mGCM',
-    ),
-    Video(
-      title: 'WIFI hacking part 3',
-      thumbnail: 'assets/img/video3_thumb.png',
-      url: 'https://www.youtube.com/watch?v=C29QstsxWQE',
-    ),
-    Video(
-      title: 'Find facebook users location',
-      thumbnail: 'assets/img/video4_thumb.png',
-      url: 'https://www.youtube.com/watch?v=J9zhKtL_gH0',
-    ),
-  ],
-);
