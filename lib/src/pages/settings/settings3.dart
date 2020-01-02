2643e9a3b2e008076079fb024db986016efc7040
Damodar Lohani <dlohani48@gmail.com>
2020-01-02 12:36:25 +0545

    image provider updated

--- lib/src/pages/settings/settings3.dart
+++ lib/src/pages/settings/settings3.dart
@@ -1,9 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 /**
  * Author: Damodar Lohani
  * profile: https://github.com/lohanidamodar
   */
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
 
@@ -44,7 +44,7 @@ class SettingsThreePage extends StatelessWidget {
                 children: <Widget>[
                   ListTile(
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage(avatars[4]),
+                      backgroundImage: CachedNetworkImageProvider(avatars[4]),
                     ),
                     title: Text("Damodar Lohani"),
                     onTap: () {},
