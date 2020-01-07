6167dacc503276300772da43f8aeeef8afb6ddfc
Damodar Lohani <dlohani48@gmail.com>
2020-01-07 15:26:00 +0545

    settings one dark mode

--- lib/src/pages/settings/settings1.dart
+++ lib/src/pages/settings/settings1.dart
@@ -8,169 +8,203 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingsOnePage extends StatelessWidget {
+class SettingsOnePage extends StatefulWidget {
   static final String path = "lib/src/pages/settings/settings1.dart";
+
+  @override
+  _SettingsOnePageState createState() => _SettingsOnePageState();
+}
+
+class _SettingsOnePageState extends State<SettingsOnePage> {
+  bool _dark;
+
+  @override
+  void initState() { 
+    super.initState();
+    _dark = false;
+  }
+
+  Brightness _getBrightness() {
+     return _dark ? Brightness.dark : Brightness.light;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
-        iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.grey.shade200,
-        title: Text(
-          'Settings',
-          style: TextStyle(color: Colors.black),
-        ),
+    return Theme(
+      isMaterialAppTheme: true,
+      data: ThemeData(
+        brightness: _getBrightness(),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          SingleChildScrollView(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Card(
-                  elevation: 8.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  color: Colors.purple,
-                  child: ListTile(
-                    onTap: () {
-                      //open edit profile
-                    },
-                    title: Text(
-                      "John Doe",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    leading: CircleAvatar(
-                      backgroundImage: CachedNetworkImageProvider(avatars[0]),
-                    ),
-                    trailing: Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                Card(
-                  elevation: 4.0,
-                  margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(
-                          Icons.lock_outline,
-                          color: Colors.purple,
-                        ),
-                        title: Text("Change Password"),
-                        trailing: Icon(Icons.keyboard_arrow_right),
-                        onTap: () {
-                          //open change password
-                        },
-                      ),
-                      _buildDivider(),
-                      ListTile(
-                        leading: Icon(
-                          FontAwesomeIcons.language,
-                          color: Colors.purple,
-                        ),
-                        title: Text("Change Language"),
-                        trailing: Icon(Icons.keyboard_arrow_right),
-                        onTap: () {
-                          //open change language
-                        },
-                      ),
-                      _buildDivider(),
-                      ListTile(
-                        leading: Icon(
-                          Icons.location_on,
-                          color: Colors.purple,
-                        ),
-                        title: Text("Change Location"),
-                        trailing: Icon(Icons.keyboard_arrow_right),
-                        onTap: () {
-                          //open change location
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                Text(
-                  "Notification Settings",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo,
-                  ),
-                ),
-                SwitchListTile(
-                  activeColor: Colors.purple,
-                  contentPadding: const EdgeInsets.all(0),
-                  value: true,
-                  title: Text("Received notification"),
-                  onChanged: (val) {},
-                ),
-                SwitchListTile(
-                  activeColor: Colors.purple,
-                  contentPadding: const EdgeInsets.all(0),
-                  value: false,
-                  title: Text("Received newsletter"),
-                  onChanged: null,
-                ),
-                SwitchListTile(
-                  activeColor: Colors.purple,
-                  contentPadding: const EdgeInsets.all(0),
-                  value: true,
-                  title: Text("Received Offer Notification"),
-                  onChanged: (val) {},
-                ),
-                SwitchListTile(
-                  activeColor: Colors.purple,
-                  contentPadding: const EdgeInsets.all(0),
-                  value: true,
-                  title: Text("Received App Updates"),
-                  onChanged: null,
-                ),
-                const SizedBox(height: 60.0),
-              ],
-            ),
+      child: Scaffold(
+        backgroundColor: _dark ? null : Colors.grey.shade200,
+        appBar: AppBar(
+          elevation: 0,
+          brightness: _getBrightness(),
+          iconTheme: IconThemeData(color: _dark ? Colors.white : Colors.black),
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Settings',
+            style: TextStyle(color: _dark ? Colors.white : Colors.black),
           ),
-          Positioned(
-            bottom: -20,
-            left: -20,
-            child: Container(
-              width: 80,
-              height: 80,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.purple,
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 00,
-            left: 00,
-            child: IconButton(
-              icon: Icon(
-                FontAwesomeIcons.powerOff,
-                color: Colors.white,
-              ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(FontAwesomeIcons.moon),
               onPressed: () {
-                //log out
+                setState(() {
+                  _dark = !_dark;
+                });
               },
+            )
+          ],
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Card(
+                    elevation: 8.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    color: Colors.purple,
+                    child: ListTile(
+                      onTap: () {
+                        //open edit profile
+                      },
+                      title: Text(
+                        "John Doe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      leading: CircleAvatar(
+                        backgroundImage: CachedNetworkImageProvider(avatars[0]),
+                      ),
+                      trailing: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    elevation: 4.0,
+                    margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(
+                            Icons.lock_outline,
+                            color: Colors.purple,
+                          ),
+                          title: Text("Change Password"),
+                          trailing: Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change password
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: Icon(
+                            FontAwesomeIcons.language,
+                            color: Colors.purple,
+                          ),
+                          title: Text("Change Language"),
+                          trailing: Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change language
+                          },
+                        ),
+                        _buildDivider(),
+                        ListTile(
+                          leading: Icon(
+                            Icons.location_on,
+                            color: Colors.purple,
+                          ),
+                          title: Text("Change Location"),
+                          trailing: Icon(Icons.keyboard_arrow_right),
+                          onTap: () {
+                            //open change location
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    "Notification Settings",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  SwitchListTile(
+                    activeColor: Colors.purple,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: true,
+                    title: Text("Received notification"),
+                    onChanged: (val) {},
+                  ),
+                  SwitchListTile(
+                    activeColor: Colors.purple,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: false,
+                    title: Text("Received newsletter"),
+                    onChanged: null,
+                  ),
+                  SwitchListTile(
+                    activeColor: Colors.purple,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: true,
+                    title: Text("Received Offer Notification"),
+                    onChanged: (val) {},
+                  ),
+                  SwitchListTile(
+                    activeColor: Colors.purple,
+                    contentPadding: const EdgeInsets.all(0),
+                    value: true,
+                    title: Text("Received App Updates"),
+                    onChanged: null,
+                  ),
+                  const SizedBox(height: 60.0),
+                ],
+              ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: -20,
+              left: -20,
+              child: Container(
+                width: 80,
+                height: 80,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 00,
+              left: 00,
+              child: IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.powerOff,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  //log out
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
