0331a6a3e9bfc3d082b259096d8d7753b9dea9ff
Damodar Lohani <dlohani48@gmail.com>
2020-04-10 15:10:22 +0545

    minimal alert dialogs

--- lib/src/pages/dialogs/dialogs.dart
+++ lib/src/pages/dialogs/dialogs.dart
@@ -2,7 +2,7 @@
  * Author: Damodar Lohani
  * profile: https://github.com/lohanidamodar
   */
-  
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
 import 'package:flutter_ui_challenges/src/widgets/network_image.dart';
@@ -10,35 +10,64 @@ import 'package:flutter_ui_challenges/src/widgets/network_image.dart';
 class DialogsPage extends StatelessWidget {
   static final String path = "lib/src/pages/dialogs/dialogs.dart";
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dialogs'),
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 10.0),
-              RaisedButton(color: Colors.green, colorBrightness: Brightness.dark,child: Text("Payment Success"),onPressed: () => _paymentSuccessDialog(context),),
-              SizedBox(height: 10.0),
-              RaisedButton(color: Colors.green, colorBrightness: Brightness.dark,child: Text("Alert Dialog"),onPressed: () => _alertDialog(context),),
-            ],
-          ),
+      body: SizedBox(
+        width: double.infinity,
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Payment Success"),
+              onPressed: () => _paymentSuccessDialog(context),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Alert Dialog"),
+              onPressed: () => _alertDialog(context),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Success Dialog"),
+              onPressed: () => _customAlertDialog(context, AlertDialogType.SUCCESS ),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Info Dialog"),
+              onPressed: () => _customAlertDialog(context, AlertDialogType.INFO ),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Warning Dialog"),
+              onPressed: () => _customAlertDialog(context, AlertDialogType.WARNING ),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen,
+              colorBrightness: Brightness.light,
+              child: Text("Error Dialog"),
+              onPressed: () => _customAlertDialog(context, AlertDialogType.ERROR ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  _paymentSuccessDialog( BuildContext context ) {
+  _paymentSuccessDialog(BuildContext context) {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return PaymentSuccessDialog();
-      }
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return PaymentSuccessDialog();
+        });
   }
 
   _alertDialog(BuildContext context) {
@@ -46,11 +75,23 @@ class DialogsPage extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return BeautifulAlertDialog();
-      }
+      },
+    );
+  }
+
+  _customAlertDialog(BuildContext context, AlertDialogType type) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomAlertDialog(
+          type: type,
+          title: "Beautiful title",
+          content: "Information to your user describing the situation.",
+        );
+      },
     );
   }
 }
-
 
 class BeautifulAlertDialog extends StatelessWidget {
   @override
@@ -63,52 +104,68 @@ class BeautifulAlertDialog extends StatelessWidget {
           padding: EdgeInsets.only(right: 16.0),
           height: 150,
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(75),
-              bottomLeft: Radius.circular(75),
-              topRight: Radius.circular(10),
-              bottomRight: Radius.circular(10)
-            )
-          ),
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75),
+                  bottomLeft: Radius.circular(75),
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10))),
           child: Row(
             children: <Widget>[
               SizedBox(width: 20.0),
-              CircleAvatar(radius: 55, backgroundColor: Colors.grey.shade200, child: PNetworkImage(infoIcon, width: 60,),),
+              CircleAvatar(
+                radius: 55,
+                backgroundColor: Colors.grey.shade200,
+                child: PNetworkImage(
+                  infoIcon,
+                  width: 60,
+                ),
+              ),
               SizedBox(width: 20.0),
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Alert!", style: Theme.of(context).textTheme.title,),
+                    Text(
+                      "Alert!",
+                      style: Theme.of(context).textTheme.title,
+                    ),
                     SizedBox(height: 10.0),
                     Flexible(
                       child: Text(
-                        "Do you want to continue to turn off the services?"),
+                          "Do you want to continue to turn off the services?"),
                     ),
                     SizedBox(height: 10.0),
-                    Row(children: <Widget>[
-                      Expanded(
-                        child: RaisedButton(
-                          child: Text("No"),
-                          color: Colors.red,
-                          colorBrightness: Brightness.dark,
-                          onPressed: (){Navigator.pop(context);},
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("No"),
+                            color: Colors.red,
+                            colorBrightness: Brightness.dark,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 10.0),
-                      Expanded(
-                        child: RaisedButton(
-                          child: Text("Yes"),
-                          color: Colors.green,
-                          colorBrightness: Brightness.dark,
-                          onPressed: (){Navigator.pop(context);},
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("Yes"),
+                            color: Colors.green,
+                            colorBrightness: Brightness.dark,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
                         ),
-                      ),
-                    ],)
+                      ],
+                    )
                   ],
                 ),
               )
@@ -134,22 +191,28 @@ class PaymentSuccessDialog extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: <Widget>[
-                Text("Thank You!",style: TextStyle(color: Colors.green),),
-                Text("Your transaction was successful",style: label,),
+                Text(
+                  "Thank You!",
+                  style: TextStyle(color: Colors.green),
+                ),
+                Text(
+                  "Your transaction was successful",
+                  style: label,
+                ),
                 Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text("DATE", style: label,),
+                    Text(
+                      "DATE",
+                      style: label,
+                    ),
                     Text("TIME", style: label)
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("2, April 2019"),
-                    Text("9:10 AM")
-                  ],
+                  children: <Widget>[Text("2, April 2019"), Text("9:10 AM")],
                 ),
                 SizedBox(height: 20.0),
                 Row(
@@ -158,12 +221,21 @@ class PaymentSuccessDialog extends StatelessWidget {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("TO", style: label,),
+                        Text(
+                          "TO",
+                          style: label,
+                        ),
                         Text("Manny Moto"),
-                        Text("manny.moto@gmail.com", style: subtitle,),
+                        Text(
+                          "manny.moto@gmail.com",
+                          style: subtitle,
+                        ),
                       ],
                     ),
-                    CircleAvatar(backgroundColor: Colors.green, backgroundImage: AssetImage(image),)
+                    CircleAvatar(
+                      backgroundColor: Colors.green,
+                      backgroundImage: AssetImage(image),
+                    )
                   ],
                 ),
                 SizedBox(height: 20.0),
@@ -173,32 +245,42 @@ class PaymentSuccessDialog extends StatelessWidget {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("AMOUNT", style: label,),
+                        Text(
+                          "AMOUNT",
+                          style: label,
+                        ),
                         Text("\$ 15000"),
                       ],
                     ),
-                    Text("COMPLETED", style: label,)
+                    Text(
+                      "COMPLETED",
+                      style: label,
+                    )
                   ],
                 ),
                 SizedBox(height: 20.0),
                 Container(
                   padding: EdgeInsets.all(10.0),
                   decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(5.0)
-                  ),
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(5.0)),
                   child: Row(
                     children: <Widget>[
-                      CircleAvatar(backgroundColor: Colors.green, child: Icon(Icons.account_balance_wallet),),
+                      CircleAvatar(
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.account_balance_wallet),
+                      ),
                       SizedBox(width: 10.0),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text("Credit/Debit Card"),
-                          Text("Master Card ending ***5", style: subtitle,),
+                          Text(
+                            "Master Card ending ***5",
+                            style: subtitle,
+                          ),
                         ],
                       )
-
                     ],
                   ),
                 )
@@ -208,5 +290,108 @@ class PaymentSuccessDialog extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+enum AlertDialogType {
+  SUCCESS,
+  ERROR,
+  WARNING,
+  INFO,
+}
+
+class CustomAlertDialog extends StatelessWidget {
+  final AlertDialogType type;
+  final String title;
+  final String content;
+  final Widget icon;
+  final String buttonLabel;
+  final TextStyle titleStyle = TextStyle(
+      fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold);
+
+  CustomAlertDialog(
+      {Key key,
+      this.title = "Successful",
+      @required this.content,
+      this.icon,
+      this.type = AlertDialogType.INFO,
+      this.buttonLabel = "Ok"})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        type: MaterialType.transparency,
+        child: Container(
+          alignment: Alignment.center,
+          child: Container(
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                icon ??
+                    Icon(
+                      _getIconForType(type),
+                      color: _getColorForType(type),
+                      size: 50,
+                    ),
+                const SizedBox(height: 10.0),
+                Text(
+                  title,
+                  style: titleStyle,
+                  textAlign: TextAlign.center,
+                ),
+                Divider(),
+                Text(
+                  content,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 40.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: FlatButton(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(buttonLabel),
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  IconData _getIconForType(AlertDialogType type) {
+    switch (type) {
+      case AlertDialogType.WARNING:
+        return Icons.warning;
+      case AlertDialogType.SUCCESS:
+        return Icons.check_circle;
+      case AlertDialogType.ERROR:
+        return Icons.error;
+      case AlertDialogType.INFO:
+      default:
+        return Icons.info_outline;
+    }
+  }
+
+  Color _getColorForType(AlertDialogType type) {
+    switch (type) {
+      case AlertDialogType.WARNING:
+        return Colors.orange;
+      case AlertDialogType.SUCCESS:
+        return Colors.green;
+      case AlertDialogType.ERROR:
+        return Colors.red;
+      case AlertDialogType.INFO:
+      default:
+        return Colors.blue;
+    }
   }
 }
