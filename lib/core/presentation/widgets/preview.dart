ee2de831f781ee153f6b64d30c9aeb0a7e1b1910
Damodar Lohani <dlohani48@gmail.com>
2020-08-24 13:57:02 +0545

    updated code viewer

--- lib/core/presentation/widgets/preview.dart
+++ lib/core/presentation/widgets/preview.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 /**
  * Author: Damodar Lohani
  * profile: https://github.com/lohanidamodar
   */
-  
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/src/pages/invitation/inauth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:share/share.dart';
 
@@ -11,66 +14,97 @@ import '../../constants.dart';
 import 'code_view.dart';
 import 'keep_alive.dart';
 
-const _TABS = <Widget>[
-  Tab(
-    child: ListTile(
-      leading: Icon(
-        Icons.code,
-        color: Colors.white,
-      ),
-      title: Text(
-        'Code',
-        style: TextStyle(color: Colors.white),
-      ),
-    ),
-  ),
-  Tab(
-    child: ListTile(
-      leading: Icon(
-        Icons.phone_android,
-        color: Colors.white,
-      ),
-      title: Text(
-        'Preview',
-        style: TextStyle(color: Colors.white),
-      ),
-    ),
-  ),
-];
-class DesignPreviewsPage extends StatelessWidget {
+class DesignPreviewsPage extends StatefulWidget {
   final String title;
   final Widget page;
   final String path;
 
-  const DesignPreviewsPage({Key key, @required this.title, @required this.page, @required this.path}) : super(key: key);
-
+  const DesignPreviewsPage(
+      {Key key, @required this.title, @required this.page, @required this.path})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return DefaultTabController(
-      length: _TABS.length,
-      child: Scaffold(
+  _DesignPreviewsPageState createState() => _DesignPreviewsPageState();
+}
+
+class _DesignPreviewsPageState extends State<DesignPreviewsPage> {
+  Offset offset;
+
+  @override
+  void initState() {
+    super.initState();
+    offset = Offset(0, 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text(
+          widget.title,
+          style: boldText.copyWith(color: Colors.black),
+        ),
         actions: <Widget>[
           IconButton(
-            icon: Icon(FontAwesomeIcons.shareSquare),
-            tooltip: "Open full preview",
-            onPressed: (){
-              Share.share('$githubRepo/blob/master/$path');
+            icon: Icon(Icons.code),
+            onPressed: () {
+              if (offset.dy == 0.9) {
+                setState(() {
+                  offset = Offset(0, 0);
+                });
+              } else {
+                setState(() {
+                  offset = Offset(0, 0.9);
+                });
+              }
+            },
+          ),
+          IconButton(
+            icon: Platform.isIOS ? Icon(Icons.ios_share) : Icon(Icons.share),
+            tooltip: "Share code",
+            onPressed: () {
+              Share.share('$githubRepo/blob/master/${widget.path}');
             },
           )
         ],
-        bottom: TabBar(
-          tabs: _TABS,
-        ),
       ),
-      body: TabBarView(
-          children: <Widget>[
-            AlwaysAliveWidget(child: MyCodeView(filePath: path,)),
-            AlwaysAliveWidget(child: page,),
-          ],
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          AlwaysAliveWidget(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 70.0),
+              child: MyCodeView(
+                filePath: widget.path,
+              ),
+            ),
+          ),
+          FractionalTranslation(
+            translation: offset,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  offset = Offset(0, 0);
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                color: Theme.of(context).scaffoldBackgroundColor,
+                padding: offset.dy == 0.9
+                    ? null
+                    : const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                child: AlwaysAliveWidget(
+                  child: widget.page,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
