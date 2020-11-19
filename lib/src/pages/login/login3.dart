0bf70d6694d887eaa1ab0218a177b558ff75e0cc
Damodar Lohani <dlohani48@gmail.com>
2020-11-19 09:05:52 +0545

    removed wave package

--- lib/src/pages/login/login3.dart
+++ lib/src/pages/login/login3.dart
@@ -2,10 +2,8 @@
  * Author: Damodar Lohani
  * profile: https://github.com/lohanidamodar
   */
-  
+
 import 'package:flutter/material.dart';
-import 'package:wave/config.dart';
-import 'package:wave/wave.dart';
 
 class LoginThreePage extends StatelessWidget {
   static final String path = "lib/src/pages/login/login3.dart";
@@ -16,27 +14,7 @@ class LoginThreePage extends StatelessWidget {
         children: <Widget>[
           Container(
             height: 650,
-            child: RotatedBox(
-              quarterTurns: 2,
-              child: WaveWidget(
-                config: CustomConfig(
-                    gradients: [
-                        [Colors.deepPurple, Colors.deepPurple.shade200],
-                        [Colors.indigo.shade200, Colors.purple.shade200],
-                    ],
-                    durations: [19440, 10800],
-                    heightPercentages: [0.20, 0.25],
-                    blur: MaskFilter.blur(BlurStyle.solid, 10),
-                    gradientBegin: Alignment.bottomLeft,
-                    gradientEnd: Alignment.topRight,
-                ),
-                waveAmplitude: 0,
-                size: Size(
-                    double.infinity,
-                    double.infinity,
-                ),
-              ),
-            ),
+            color: Colors.pink,
           ),
           ListView(
             children: <Widget>[
@@ -45,50 +23,64 @@ class LoginThreePage extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("Login", textAlign: TextAlign.center, style: TextStyle(
-                      color: Colors.white70,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0
-                    )),
+                    Text("Login",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28.0)),
                     Card(
-                      margin: EdgeInsets.only(left: 30, right:30, top:30),
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 30),
                       elevation: 11,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
                       child: TextField(
                         decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.person, color: Colors.black26,),
-                          suffixIcon: Icon(Icons.check_circle, color: Colors.black26,),
-                          hintText: "Username",
-                          hintStyle: TextStyle(color: Colors.black26),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                          ),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0)
-                        ),
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.black26,
+                            ),
+                            suffixIcon: Icon(
+                              Icons.check_circle,
+                              color: Colors.black26,
+                            ),
+                            hintText: "Username",
+                            hintStyle: TextStyle(color: Colors.black26),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0)),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 16.0)),
                       ),
                     ),
                     Card(
-                      margin: EdgeInsets.only(left: 30, right:30, top:20),
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 20),
                       elevation: 11,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
                       child: TextField(
                         decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock, color: Colors.black26,),
-                          hintText: "Password",
-                          hintStyle: TextStyle(
-                            color: Colors.black26,
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                          ),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0)
-                        ),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Colors.black26,
+                            ),
+                            hintText: "Password",
+                            hintStyle: TextStyle(
+                              color: Colors.black26,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0)),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 16.0)),
                       ),
                     ),
                     Container(
@@ -97,62 +89,78 @@ class LoginThreePage extends StatelessWidget {
                       child: RaisedButton(
                         padding: EdgeInsets.symmetric(vertical: 16.0),
                         color: Colors.pink,
-                        onPressed: (){},
+                        onPressed: () {},
                         elevation: 11,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                        child: Text("Login", style: TextStyle(
-                          color: Colors.white70
-                        )),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(40.0))),
+                        child: Text("Login",
+                            style: TextStyle(color: Colors.white70)),
                       ),
                     ),
-                    Text("Forgot your password?", style: TextStyle(
-                      color: Colors.white
-                    ))
+                    Text("Forgot your password?",
+                        style: TextStyle(color: Colors.white))
                   ],
                 ),
               ),
-              SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+              ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     Text("or, connect with"),
-                    SizedBox(height: 20.0,),
+                    SizedBox(
+                      height: 20.0,
+                    ),
                     Row(
                       children: <Widget>[
-                        SizedBox(width: 20.0,),
+                        SizedBox(
+                          width: 20.0,
+                        ),
                         Expanded(
                           child: RaisedButton(
                             child: Text("Facebook"),
                             textColor: Colors.white,
                             color: Colors.blue,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40)),
                             ),
-                            onPressed: (){},
+                            onPressed: () {},
                           ),
                         ),
-                        SizedBox(width: 10.0,),
+                        SizedBox(
+                          width: 10.0,
+                        ),
                         Expanded(
                           child: RaisedButton(
                             child: Text("Twitter"),
                             textColor: Colors.white,
                             color: Colors.indigo,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40)),
                             ),
-                            onPressed: (){},
+                            onPressed: () {},
                           ),
                         ),
-                        SizedBox(width: 20.0,),
+                        SizedBox(
+                          width: 20.0,
+                        ),
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text("Dont have an account?"),
-                        FlatButton(child: Text("Sign up"), textColor: Colors.indigo, onPressed: (){},)
+                        FlatButton(
+                          child: Text("Sign up"),
+                          textColor: Colors.indigo,
+                          onPressed: () {},
+                        )
                       ],
                     )
                   ],
