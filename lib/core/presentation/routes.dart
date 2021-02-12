d19e0288807a84515e19380902d8b564f9daa3fb
Aparna Dulal <55906788+Ambikadulal@users.noreply.github.com>
2021-02-12 22:22:02 +0545

    Motorbike (#72)
    * changes upto
    
    * DiscoveryPage
    
    * Moterbike Shop UI
    
    Co-authored-by: Aparna16 <aparna@kumarijob.com>

--- lib/core/presentation/routes.dart
+++ lib/core/presentation/routes.dart
@@ -7,6 +7,7 @@ import 'package:flutter_ui_challenges/src/pages/animations/anim4.dart';
 import 'package:flutter_ui_challenges/src/pages/animations/anim5.dart';
 import 'package:flutter_ui_challenges/src/pages/lists/grid_view.dart';
 import 'package:flutter_ui_challenges/src/pages/bike/bike_details.dart';
+import 'package:flutter_ui_challenges/src/pages/bike/bikeshop_ui.dart';
 import 'package:flutter_ui_challenges/src/pages/bike/home_page.dart';
 import 'package:flutter_ui_challenges/src/pages/blog/news1.dart';
 import 'package:flutter_ui_challenges/src/pages/blog/sports_news1.dart';
@@ -201,6 +202,7 @@ final List<dynamic> pages = [
     ],
   ),
   MenuItem(title: "Motorbike App", icon: Icons.list, items: [
+    SubMenuItem("MoterBike Shop Page", MoterBikeShopPage(), path: MoterBikeShopPage.path),
     SubMenuItem("Home Page", BikeHomePage(), path: BikeHomePage.path),
     SubMenuItem("Bike Details Page", BikeDetailsPage(),
         path: BikeDetailsPage.path),
