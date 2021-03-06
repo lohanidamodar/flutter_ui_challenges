a54727190b601bf38b783f33e187df9f66976e22
Aparna16 <aparna@kumarijob.com>
2021-03-06 17:25:14 +0545

    food develivery page

--- lib/src/pages/food/food_delivery.dart
+++ lib/src/pages/food/food_delivery.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class FoodDeliveryPage extends StatefulWidget {
-    static final String path = "lib/src/pages/food/food_delivery.dart";
+  static final String path = "lib/src/pages/food/food_delivery.dart";
   @override
   _FoodDeliveryPageState createState() => _FoodDeliveryPageState();
 }
@@ -24,7 +24,10 @@ class _FoodDeliveryPageState extends State<FoodDeliveryPage> {
         actions: <Widget>[
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.shopping_basket, color: Colors.grey[800],),
+            icon: Icon(
+              Icons.shopping_basket,
+              color: Colors.grey[800],
+            ),
           )
         ],
       ),
@@ -37,38 +40,75 @@ class _FoodDeliveryPageState extends State<FoodDeliveryPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeAnimation(1, Text('Food Delivery', style: TextStyle(color: Colors.grey[80], fontWeight: FontWeight.bold, fontSize: 30),)),
-                  SizedBox(height: 20,),
+                  FadeAnimation(
+                      1,
+                      Text(
+                        'Food Delivery',
+                        style: TextStyle(
+                            color: Colors.grey[80],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Container(
                     height: 50,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        FadeAnimation(1,   makeCategory(isActive: true, title: 'Pizaa')),
-                        FadeAnimation(1.3, makeCategory(isActive: false, title: 'Burgers')),
-                        FadeAnimation(1.4, makeCategory(isActive: false, title: 'Kebab')),
-                        FadeAnimation(1.5, makeCategory(isActive: false, title: 'Desert')),
-                        FadeAnimation(1.6, makeCategory(isActive: false, title: 'Salad')),
+                        FadeAnimation(
+                            1, makeCategory(isActive: true, title: 'Pizaa')),
+                        FadeAnimation(1.3,
+                            makeCategory(isActive: false, title: 'Burgers')),
+                        FadeAnimation(
+                            1.4, makeCategory(isActive: false, title: 'Kebab')),
+                        FadeAnimation(1.5,
+                            makeCategory(isActive: false, title: 'Desert')),
+                        FadeAnimation(
+                            1.6, makeCategory(isActive: false, title: 'Salad')),
                       ],
                     ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                 ],
               ),
             ),
-            FadeAnimation(1, Padding(
-              padding: EdgeInsets.all(20),
-              child: Text('Free Delivery', style: TextStyle(color: Colors.grey[700], fontSize: 20, fontWeight: FontWeight.bold),),
-            )),
+            FadeAnimation(
+                1,
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'Free Delivery',
+                    style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )),
             Expanded(
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20.0),
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: <Widget>[
-                    FadeAnimation(1.6, makeItem(image: 'https://cdn.pixabay.com/photo/2017/08/06/06/43/pizza-2589575__340.jpg')),
-                    FadeAnimation(1.5, makeItem(image: 'https://cdn.pixabay.com/photo/2014/07/08/12/34/pizza-386717__340.jpg')),
-                    FadeAnimation(1.6, makeItem(image: 'https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505__340.jpg')),
+                    FadeAnimation(
+                        1.6,
+                        makeItem(
+                            image:
+                                'https://cdn.pixabay.com/photo/2017/08/06/06/43/pizza-2589575__340.jpg')),
+                    FadeAnimation(
+                        1.5,
+                        makeItem(
+                            image:
+                                'https://cdn.pixabay.com/photo/2014/07/08/12/34/pizza-386717__340.jpg')),
+                    FadeAnimation(
+                        1.6,
+                        makeItem(
+                            image:
+                                'https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505__340.jpg')),
                   ],
                 ),
               ),
@@ -92,37 +132,40 @@ class _FoodDeliveryPageState extends State<FoodDeliveryPage> {
           borderRadius: BorderRadius.circular(50),
         ),
         child: Align(
-          child: Text(title, style: TextStyle(color: isActive ? Colors.white : Colors.grey[900], fontSize: 18, fontWeight: isActive ? FontWeight.bold : FontWeight.w100),),
+          child: Text(
+            title,
+            style: TextStyle(
+                color: isActive ? Colors.white : Colors.grey[900],
+                fontSize: 18,
+                fontWeight: isActive ? FontWeight.bold : FontWeight.w100),
+          ),
         ),
       ),
     );
   }
 
-  Widget  makeItem({image}) {
+  Widget makeItem({image}) {
     return AspectRatio(
       aspectRatio: 1 / 1.5,
       child: GestureDetector(
         child: Container(
           margin: EdgeInsets.only(right: 20),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: NetworkImage(image),
-              fit: BoxFit.cover,
-            )
-          ),
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: NetworkImage(image),
+                fit: BoxFit.cover,
+              )),
           child: Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                stops: [.2, .9],
-                colors: [
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(begin: Alignment.bottomCenter, stops: [
+                  .2,
+                  .9
+                ], colors: [
                   Colors.black.withOpacity(.9),
                   Colors.black.withOpacity(.3),
-                ]
-              )
-            ),
+                ])),
             child: Padding(
               padding: EdgeInsets.all(20.0),
               child: Column(
@@ -131,14 +174,28 @@ class _FoodDeliveryPageState extends State<FoodDeliveryPage> {
                 children: <Widget>[
                   Align(
                     alignment: Alignment.topRight,
-                    child: Icon(Icons.favorite, color: Colors.white,),
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    ),
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("\$ 20.00", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 10,),
-                      Text("Vegetarian Pizza", style: TextStyle(color: Colors.white, fontSize: 20),)
+                      Text(
+                        "\$ 20.00",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Vegetarian Pizza",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
                     ],
                   )
                 ],
@@ -150,6 +207,7 @@ class _FoodDeliveryPageState extends State<FoodDeliveryPage> {
     );
   }
 }
+
 class FadeAnimation extends StatelessWidget {
   final double delay;
   final Widget child;
@@ -158,13 +216,11 @@ class FadeAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tween = MultiTrackTween([
-      Track('opacity').add(Duration(milliseconds: 500), 
-        Tween(begin: 0.0, end: 1.0)
-      ),
-      Track('translateX').add(Duration(milliseconds: 500), 
-        Tween(begin: 120.0, end: 0.0),
-        curve: Curves.easeOut
-      )
+      Track('opacity')
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track('translateX').add(
+          Duration(milliseconds: 500), Tween(begin: 120.0, end: 0.0),
+          curve: Curves.easeOut)
     ]);
     return ControlledAnimation(
       delay: Duration(milliseconds: (500 * delay).round()),
